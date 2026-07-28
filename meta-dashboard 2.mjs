af4d39d99c9531198d4d36dae1d4836c585a6c3b
leticiaguimaraes081225-crypto <leticiaguimaraes081225@gmail.com>
2026-07-27 22:13:11 -0300

    Delete meta-dashboard 2.mjs

--- meta-dashboard 2.mjs
+++
@@ -1,7 +0,0 @@
-import handler from "./meta.mjs";
-
-export default async request => {
-  const url = new URL(request.url);
-  url.searchParams.set("mode", "dashboard");
-  return handler(new Request(url.toString(), request));
-};
